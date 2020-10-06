cdbef5fc1b5d14d437b3ba73ac64308f30275c2e
Mate Soos <soos.mate@gmail.com>
2020-10-06 21:01:44 +0200

    Fixing test, improve CMakeLists

--- scripts/todo.sh
+++ scripts/todo.sh
@@ -4,6 +4,6 @@ set -x
 
 rm -f graph.txt graph.weights graph.bin
 
-./mywork.py $1
-./convert -i graph.txt -o graph.bin -w graph.weights
-./louvain  graph.bin -w graph.weights -v -e 0.001 -l 100
+./mytest.py $1
+./comml-convert -i graph.txt -o graph.bin -w graph.weights
+./comml-louvain  graph.bin -w graph.weights -v -e 0.001 -l 100
