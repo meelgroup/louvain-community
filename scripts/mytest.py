59ee53325edc0e9ede74e83a478c7eebfa62f2e0
Mate Soos <soos.mate@gmail.com>
2020-10-11 12:26:07 +0200

    Adding license

--- scripts/mytest.py
+++ scripts/mytest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2009-2020 Authors of CryptoMiniSat, see AUTHORS file
+# Copyright (C) 2020 Mate Soos
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
